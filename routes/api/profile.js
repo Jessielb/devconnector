b9a7e5f6d04515c9f7386045f857efd972d81383
Jessielb <jessielb75@gmail.com>
2022-06-15 21:13:15 -0400

    Added routes and functionality for education within profile; also added ability to get github repos within profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -261,6 +263,131 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
             res.status(500).send('Server Error');
     }
 });
+
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put(
+    '/education', 
+    [
+        auth, 
+        [
+            check('school', 'School is required')
+                .not()
+                .isEmpty(),
+            check('degree', 'Degree is required')
+                .not()
+                .isEmpty(),
+            check('fieldofstudy', 'Field of study is required')
+                .not()
+                .isEmpty(),
+            check('from', 'From date is required')
+                .not()
+                .isEmpty()
+        ]
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newEdu = { // creates object with the data the user submits
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        }
+
+        // now we deal with mongodb
+        try {
+            // profile we wanna add the education to. which we get from the user token
+            const profile = await Profile.findOne({ user: req.user.id });
+
+            // education is an array, and unshift is to push to the beginning of it; so we add the new edu to it
+            profile.education.unshift(newEdu);
+
+            await profile.save();
+
+            res.json(profile);
+        } 
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        // current logged user's profile
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get the index for the education we want to remove
+        const removeIndex = profile.education
+            .map(item => item.id)
+            .indexOf(req.params.edu_id);
+
+        // at positive removeIndex in education array, remove 1 item (splice)
+        profile.education.splice(removeIndex, 1);
+
+        // save the changes
+        await profile.save();
+
+        // response
+        res.json(profile);
+    }
+    catch (err) {
+        console.error(err.message);
+            res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if(response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No Github profile found'});
+            }
+
+            res.json(JSON.parse(body));
+        });
+    }
+    catch (err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 
 
